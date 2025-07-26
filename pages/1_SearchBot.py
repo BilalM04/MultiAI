e0d9ff48080d8fa40851784fab4a24e51438a23e
Mohammad Bilal <bilal71612@gmail.com>
2025-07-26 15:41:32 -0400

    pass full history to SearchBot

--- pages/1_SearchBot.py
+++ pages/1_SearchBot.py
@@ -62,7 +62,7 @@ if prompt and len(prompt) <= 500:
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
         try:
-            response = search_agent.run(prompt, callbacks=[st_cb])
+            response = search_agent.run(st.session_state.search_messages, callbacks=[st_cb])
             st.session_state.search_messages.append({"role": "assistant", "content": response})
             st.write(response)
         except Exception as e:
